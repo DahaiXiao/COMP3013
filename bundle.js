e712fa1d5aaf2e2d5f0cb6c346aa7645d899168c
Arwen Sean <it@lahoo.ca>
2023-10-14 12:48:26 -0700

    Add files via upload

---
+++ bundle.js
@@ -0,0 +1,63 @@
+
+> assignment-tracker@0.0.0 build
+> tsc && vite build -
+
+src/App.tsx(1,33): error TS2307: Cannot find module 'react' or its corresponding type declarations.
+src/App.tsx(4,1): error TS6133: 'Assignment' is declared but its value is never read.
+src/App.tsx(4,28): error TS2307: Cannot find module '../Assignment' or its corresponding type declarations.
+src/App.tsx(33,5): error TS2307: Cannot find module 'react/jsx-runtime' or its corresponding type declarations.
+src/components/Assignment/index.tsx(2,33): error TS2307: Cannot find module 'react' or its corresponding type declarations.
+src/components/Assignment/index.tsx(3,20): error TS2307: Cannot find module './assignment.module.css' or its corresponding type declarations.
+src/components/Assignment/index.tsx(4,25): error TS2307: Cannot find module 'react-icons/tb' or its corresponding type declarations.
+src/components/Assignment/index.tsx(5,39): error TS2307: Cannot find module 'react-icons/bs' or its corresponding type declarations.
+src/components/Assignment/index.tsx(14,10): error TS6133: 'isdeleted' is declared but its value is never read.
+src/components/Assignment/index.tsx(31,5): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignment/index.tsx(31,5): error TS2307: Cannot find module 'react/jsx-runtime' or its corresponding type declarations.
+src/components/Assignment/index.tsx(32,7): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignment/index.tsx(35,26): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignment/index.tsx(37,7): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignment/index.tsx(39,7): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignment/index.tsx(39,83): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignment/index.tsx(41,7): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignment/index.tsx(43,7): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignment/index.tsx(44,5): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(1,19): error TS2307: Cannot find module 'react' or its corresponding type declarations.
+src/components/Assignments/index.tsx(3,20): error TS2307: Cannot find module './assignments.module.css' or its corresponding type declarations.
+src/components/Assignments/index.tsx(16,5): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(16,5): error TS2307: Cannot find module 'react/jsx-runtime' or its corresponding type declarations.
+src/components/Assignments/index.tsx(17,7): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(18,9): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(19,11): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(19,33): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(20,11): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(20,40): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(21,9): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(23,9): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(24,11): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(24,65): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(25,11): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(25,66): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(26,9): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(27,7): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(29,7): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(31,23): error TS2322: Type '{ key: number; assignmentname: string; removeAssignment: () => void; addComplete: (completedAssignment: string) => void; }' is not assignable to type 'AssignmentProps'.
+  Property 'key' does not exist on type 'AssignmentProps'.
+src/components/Assignments/index.tsx(37,7): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Assignments/index.tsx(38,5): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Header/index.tsx(1,33): error TS2307: Cannot find module 'react' or its corresponding type declarations.
+src/components/Header/index.tsx(2,20): error TS2307: Cannot find module './header.module.css' or its corresponding type declarations.
+src/components/Header/index.tsx(3,37): error TS2307: Cannot find module 'react-icons/ai' or its corresponding type declarations.
+src/components/Header/index.tsx(18,5): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Header/index.tsx(18,5): error TS2307: Cannot find module 'react/jsx-runtime' or its corresponding type declarations.
+src/components/Header/index.tsx(19,7): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Header/index.tsx(19,49): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Header/index.tsx(20,7): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Header/index.tsx(22,20): error TS7006: Parameter 'e' implicitly has an 'any' type.
+src/components/Header/index.tsx(27,9): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Header/index.tsx(33,9): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Header/index.tsx(33,153): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Header/index.tsx(34,7): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/components/Header/index.tsx(35,5): error TS7026: JSX element implicitly has type 'any' because no interface 'JSX.IntrinsicElements' exists.
+src/main.tsx(1,19): error TS2307: Cannot find module 'react' or its corresponding type declarations.
+src/main.tsx(2,22): error TS2307: Cannot find module 'react-dom/client' or its corresponding type declarations.
+src/main.tsx(7,3): error TS2307: Cannot find module 'react/jsx-runtime' or its corresponding type declarations.
